7b47e98ebe828143f76f783f57715d0a30851e70
Valentin <Ponsonnetvalentin@gmail.com>
2021-11-03 15:18:19 +0100

    Lotto V.2

--- script.js
+++ script.js
@@ -24,10 +24,66 @@ console.log(sommeNombreS(nombrea, nombreb));
 console.log(sommeNombreM(nombrea, nombreb));
 console.log(sommeNombreD(nombrea, nombreb)); */
 
-//Faire un random avec Math.floor et Math.random
+//Faire un random avec Math.floor et Math.random / Lotto V.1
 
-const Participant = ["Romain", "Antoine","Kevin","Anthony", "Lucie", "Valentin"];
+/* const Participant = ["Romain", "Antoine","Kevin","Anthony", "Lucie", "Valentin"];
 
 const rmd = Participant[Math.floor(Math.random() * Participant.length)];
 
-console.log(rmd);
+console.log(rmd); */
+
+/* //Lotto V.2
+//Les résultats du Lotto
+const ResultatLotto = [10, 9, 12, 50, 61];
+// Tableau des tickets
+const Ticket = [50, 7, 12, 24, 11];
+// Variable de travail
+let match = 0;
+// On créer une fonction qui compare avec 3 entrer (défini dans la parenthèse)
+function compare(res, ticket, nom){
+// On créer une première boucle for qui boucle sur le premier tableau
+    for(i=0;i<res.length;i++){
+        // Creation d'une pute
+        let check = res[i];
+        // On créer une deuxièmes boucles for boucle sur le deuxième tableau
+        for(j=0;j<ticket.length;j++){
+            // On verifie s'il y a correspondance
+            if(check == ticket[j]){
+                // On incrémente de 1 à chaque correspondance
+                match++;
+                
+            }
+
+        };
+
+    };
+
+    //console.log(match)
+    // Créer les conditions
+    if(match==1){
+        let result = nom + " à gagner 100$, parce qu'il a eu 1 numéro !"
+        console.log(result)
+    }
+    else if(match==2){
+        let result = nom + " à gagner 400$, parce qu'il a eu 2 numéros !"
+        console.log(result)
+    }
+    else if(match==3){
+        let result = nom + " à gagner 600$, parce qu'il a eu 3 numéros !"
+        console.log(result)
+    }
+    else if(match==4){
+        let result = nom + " à gagner 800$, parce qu'il a eu 4 numéros !"
+        console.log(result)
+    }
+    else if(match==5){
+        let result = nom + " à gagner 1000$, parce qu'il a eu 5 numéros !"
+        console.log(result)
+    }
+    else {
+        console.log("Aucun Match !")
+    };
+};
+
+compare(ResultatLotto, Ticket, "Thomas")
+compare(ResultatLotto, Ticket, "Jean") */
